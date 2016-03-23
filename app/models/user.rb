28b6b43419e08f1a6ad4a60acc6efffa453d50cb
Andrew Baik <and.baik@gmail.com>
2016-03-23 16:22:42 -0400

    added user signup form and methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
   has_many :favorites
   has_many :trucks, through: :favorites
+  has_secure_password
+
+  before_create :generate_token
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def generate_token
+    self.auth_token = SecureRandom.uuid + "-" +  Digest::MD5.hexdigest("#{email}")
+  end
+
+  def regenerate_auth_token
+    self.auth_token = nil
+    generate_token
+    save!
+  end
 end
