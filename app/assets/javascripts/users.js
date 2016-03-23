e2431d9c897eafc97b315dc64462b8d72987fdba
Andrew Baik <and.baik@gmail.com>
2016-03-23 19:27:31 -0400

    googlemap integration in progress

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,41 +1,65 @@
-var longitude, latitude;
+function initAutocomplete() {
+  var sanFrancisco = new google.maps.LatLng(37.7833, -122.4167);
+  //var url = "https://data.sfgov.org/resource/6a9r-agq8.json";
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.setCoords(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function setCoords(position) {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
-}
-
-$(window).load(function() {
-  getLocation();
-
-  url = 'http://data.ct.gov/resource/9k2y-kqxn.json?organization_type=Public%20School%20Districts&$$app_token=CGxaHQoQlgQSev4zyUh5aR5J3';
-
-  // Intialize our map
-  var center = new google.maps.LatLng(41.7656874,-72.680087);
-  var mapOptions = {
-    zoom: 8,
-    center: center
-  };
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  // Retrieve our data and plot it
-  $.getJSON(url, function(data, textstatus) {
-      console.log(data);
-      $.each(data, function(i, entry) {
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(entry.location_1.latitude,
-            entry.location_1.longitude),
-          map: map,
-          title: location.name
-        });
-      });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: sanFrancisco,
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   });
-});
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
